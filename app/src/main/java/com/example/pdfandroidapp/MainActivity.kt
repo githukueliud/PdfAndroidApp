225567c52ed50c8ea59832345d321c0960de2ee7
Eliud Githuku <githukueliud@gmail.com>
2023-10-14 13:59:53 +0300

    Create pdf and saving functionality done

--- app/src/main/java/com/example/pdfandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/pdfandroidapp/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.pdfandroidapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.pdfandroidapp.ui.theme.PdfAndroidAppTheme
+import com.itextpdf.kernel.geom.PageSize
+import com.itextpdf.kernel.pdf.PdfDocument
+import com.itextpdf.kernel.pdf.PdfWriter
+import com.itextpdf.layout.Document
+import com.itextpdf.layout.element.Paragraph
+import java.io.File
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +30,34 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //app entry comes here
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Button(onClick = { createPdf(this@MainActivity) }) {
+                            Text(text = "Save")
+                        }
+                    }
                 }
             }
         }
     }
+}
+
+
+
+private fun createPdf(context: Context) {
+    val outputFile = File(context.filesDir, "test.pdf")
+    val pdfWriter = PdfWriter(outputFile)
+    val pdfDocument = PdfDocument(pdfWriter)
+    val document = Document(pdfDocument, PageSize.A3)
+    pdfDocument.addNewPage()
+
+    document.add(
+        Paragraph("Hello world")
+    )
+
+    document.close()
+
 }
 
